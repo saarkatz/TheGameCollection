11f1c6a29fb61bb7eaf221dcbff7a93271170cd1
Saar Katz <kats.saar@gmail.com>
2019-08-24 22:01:59 +0300

    Reflow python_backend module docstring

--- engine/backend/pygame_backend/__init__.py
+++ engine/backend/pygame_backend/__init__.py
@@ -1,4 +1,5 @@
 """
-This package is responsible for wrapping pygame into the structures required by our game engine.
+This package is responsible for wrapping pygame into the structures
+required by our game engine.
 """
 from pygame import *
