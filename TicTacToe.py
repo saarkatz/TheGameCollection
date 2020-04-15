771167af8dd2ed75dd33cd16923fa046f1925ac4
Galit <galamatan148@gmail.com>
2020-04-15 19:20:10 +0300

    ???

--- TicTacToe.py
+++ TicTacToe.py
@@ -36,36 +36,65 @@ PLAYER_COLORS = (NOPLAYER_COLOR, PLAYER1_COLOR, PLAYER2_COLOR)
 
 # TODO: ask players what are they're names, and print the winners name at the end (instead of "player 1/2")
 WINNING_MESSAGE = 'Congratulations {0} you are the winner!!!'
+FONT = 'Comic Sans MS'
+FONT_SIZE = 15
 
 
 # TODO: understand why this function doesn't work...
 # Check if current player won the game
-def is_win(player, board, curr_row, curr_col):
+def is_win(curr_player, board, curr_row, curr_col):
+    """
+    check if one of the players won
+    :param curr_player: current player (1 or 2)
+    :param board: current board state
+    :param curr_row: current row
+    :param curr_col: current column
+    :return: True if winn, False if not
+    """
     curr_color = PLAYER1_COLOR
-    if player == 1:
+    if curr_player == PLAYER2:
         curr_color = PLAYER2_COLOR
 
-    # print(curr_color)
-    # print()
-
-    # check if a player finished a row
+    # count number of cells in a row/ column of the same player
     count = 0
+
+    # check current row
     for column in range(COLUMNS):
         if not board[curr_row][column] == curr_color:
-            # print(board[curr_row][column])
-            count += 1
             break
-    if count == 3:
+        else:
+            count += 1
+
+    # if all cell in the row where the player's color - the player won
+    if count == COLUMNS:
+        print("wow!!!")
         return True
+
+    # now - check column
     count = 0
     for row in range(ROWS):
         if not board[curr_col][row] == curr_color:
-            count += 1
             break
-    if count == 3:
+        else:
+            count += 1
+
+    # if all cell in the column where the player's color - the player won
+    if count == ROWS:
+        print("wowww!!!")
         return True
 
     return False
+
+
+def update_screen(screen, board, board_view):
+    # Draw the board
+    screen.fill(PURPLE)
+    pygame.draw.rect(screen, BLACK, [TOPRIGHT_CORNER[0], TOPRIGHT_CORNER[1],
+                                     BOARD_SIZE[0], BOARD_SIZE[1]])
+    for i in range(ROWS):
+        for j in range(COLUMNS):
+            pygame.draw.rect(screen, PLAYER_COLORS[board[i][j]], board_view[i][j])
+    pygame.display.update()
 
 
 # The main function. Contains the main loop of the game
@@ -76,7 +105,9 @@ def main():
     pygame.display.set_caption(CAPTION)
 
     # The model of the board
-    board = [[NOPLAYER] * ROWS] * COLUMNS
+    board = [
+        [NOPLAYER for _ in range(COLUMNS)] for _ in range(ROWS)
+    ]
 
     # The view of the board
     board_view = [
@@ -108,10 +139,9 @@ def main():
                 # TODO: why is it like that?
                 for i in range(ROWS):
                     for j in range(COLUMNS):
-                        print(i, j, board[i][j], board_view[i][j], board_view[i][j].collidepoint((115, 375)))
                         player = turn % 2
 
-                        if board[i][j] == NOPLAYER and board_view[i][j].collidepoint((115, 375)):
+                        if board[i][j] == NOPLAYER and board_view[i][j].collidepoint(pygame.mouse.get_pos()):
                             if player == 1:
                                 board[i][j] = PLAYER1
                             else:
@@ -124,7 +154,7 @@ def main():
                                 else:
                                     screen.fill(PLAYER2_COLOR)
                                     winner = "player 2"
-                                winning_font = pygame.font.SysFont('Comic Sans MS', 15)
+                                winning_font = pygame.font.SysFont(FONT, FONT_SIZE)
                                 winning_text = winning_font.render(WINNING_MESSAGE.format(winner), False, (0, 0, 0))
                                 screen.blit(winning_text, (0, 0))
                                 pygame.display.update()
@@ -133,17 +163,9 @@ def main():
 
             # show regular updated board
             if not victory:
-                # Draw the board
-                screen.fill(PURPLE)
-                pygame.draw.rect(screen, BLACK, [TOPRIGHT_CORNER[0], TOPRIGHT_CORNER[1],
-                                                 BOARD_SIZE[0], BOARD_SIZE[1]])
-                for i in range(ROWS):
-                    for j in range(COLUMNS):
-                        pygame.draw.rect(screen, PLAYER_COLORS[board[i][j]], board_view[i][j])
-                pygame.display.update()
+                update_screen(screen, board, board_view)
 
     finally:
-        # todo: maybe print something before quiting
         pygame.quit()
 
 
